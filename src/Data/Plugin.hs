e2a262512363c6e05af3f309ea10a6843c606f2a
michaeljklein <michael14486@gmail.com>
2016-05-19 17:14:02 -0400

    scratch work for pipes

--- src/Data/Plugin.hs
+++ src/Data/Plugin.hs
@@ -1,24 +1,8 @@
 module Data.Plugin where
 
 
-
--- So a command line option has the following featurs:
--- command line interface (parser for options)
--- does work
--- outputs Attribute
-
-
 mapToMap :: Ord a => (a -> b) -> [a] -> M.Map a b
 mapToMap f = fromList . map (fmap f . join (,))
-
-
--- main question: how to organize the fetchers?
--- Don't want to fetch multiple times
--- Don't want to wait for plugins
---
--- question then is: how much power to give to the plugins?
---  If plugins have `fileWanted :: InitialDetails -> Commit -> Path -> Bool`, will be extremely fast
-
 
 -- Example plugins:
 --  char counter
@@ -31,42 +15,243 @@ mapToMap f = fromList . map (fmap f . join (,))
 --  Conclusion: Do NOT allow plugins multiple requests or anything other than a stream filter
 
 
--- Plugin:
---  parse :: Parser a
---  filesWanted :: FilesWanted
---  pipe :: a -> (FilesGiven + State) -> Producer [Attribute]
---
-
 {-# DEPRECATED Mode "Need to update this type for cmdargs" #-}
 data Mode a
 
-data Plugin s a = Plug { plugName :: Text                                                      -- Plugin name
-                       , parser   :: Mode a                                                    -- Command-line mode
-                       , wanted   :: Details -> a -> Wanted                                    -- What files/details the plugin wants
-                       , plugPipe :: Details -> Pipe (LocalInput, LocalState a) (LocalState a) -- The main pipe of the plugin, consuming local state/input and producing localstate
+type PluginPipe s t = Pipe (LocalInput s) (LocalState t)
+
+-- | The `Plugin` data type contains everything that a plugin needs to be
+-- integrated into the tool (including lenses)
+data Plugin s a = Plug { _plugName    :: Text                                      -- Plugin name
+                       , _parser      :: Mode a                                    -- Command-line mode
+                       , _wanted      :: Details -> a -> Wanted                    -- What files/details the plugin wants
+                       , _plugMapper  :: Details -> a -> PluginPipe s s            -- The main pipe of the plugin, consuming local state/input and producing localstate
+                       , _plugReducer :: Details -> a -> Maybe (PluginPipe [s] s)  -- If `Just`, plugin will be run in mapReduce fashion
                        }
 
-data LocalInput = LocalInput { commit   :: Maybe Commit
-                             , diffTree :: Maybe DiffTree
-                             , dirTree  :: Maybe DirTree
-                             , files    :: Maybe Map Path Text
+makeClassy ''Plugin
+
+
+
+
+instance Ord (Plugin s a) where
+  compare x y = compare (x^.plugName) (y^.plugName)
+
+
+-- | This is what a `Plugin` pipe can access. It's updated and fed back into
+-- the pipe at each step
+data LocalInput a = LocInput { _commit         :: Maybe Commit
+                             , _commitDiffTree :: Maybe DiffTree
+                             , _commitDirTree  :: Maybe DirTree
+                             , _commitFiles    :: Maybe Map Path Text
+                             , _localState     :: LocalState a
                              }
 
-data LocalState a = LocalState { state :: a
-                               , attributes :: [Attribute]
+makeClassy ''LocalInput
+
+
+-- | This is the local state for a `Plugin`
+data LocalState a = LocalState { _state :: a
+                               , _attributes :: [Attribute]
                                }
 
-data Wanted = Want { diffTree        :: Bool                    -- ^ The `DiffTree` results
-                   , dirTree         :: Bool                    -- ^ The `DirectoryTree`
-                   , files           :: Bool                    -- ^ File results (a plugin could only access diffs or the trees)
-                   , byCommit        :: Maybe (Commit -> Bool)  -- ^ Filter by `Commit`
-                   , byCommitHash    :: Maybe (SHA1 -> Bool)    -- ^ Filter by `SHA1` hash
-                   , byCommitDate    :: Maybe (UTCTime -> Bool) -- ^ Filter by `UTCTime` date
-                   , byFileExtension :: Maybe (Text -> Bool)    -- ^ Filter by file extension
-                   , byFileName      :: Maybe (Text -> Bool)    -- ^ Filter by full file name
-                   , byDirectoryPath :: Maybe (Path -> Bool)    -- ^ Filter by directory path
-                   , byDiffTree      :: Maybe (DiffTree -> Bool)-- ^ Filter by the `DiffTree`
+makeClassy ''LocalState
+
+
+-- | This allows a plugin to finely specify what it wants from the project
+-- and how it's choosing. Because of how fine this is, fetchers may quickly
+-- filter the files/commits for piping to the plugins. Note: all the given
+-- @by_@ parameters must return `True` for the info to be passed to the plugin.
+-- If diffTree, dirTree, files are all false, only (Details, Commit) will be
+-- passed to the plugin.
+data Wanted = Want { _diffTree        :: Bool                    -- ^ The `DiffTree` results
+                   , _dirTree         :: Bool                    -- ^ The `DirectoryTree`
+                   , _files           :: Bool                    -- ^ File results (a plugin could only access diffs or the trees)
+                   , _wantBy          :: WantedBy
                    }
+
+data WantedBy = WantBy { _byCommit        :: Maybe (Commit -> Bool)  -- ^ Filter by `Commit`
+                       , _byFileExtension :: Maybe (Text -> Bool)    -- ^ Filter by file extension
+                       , _byFileName      :: Maybe (Text -> Bool)    -- ^ Filter by full file name
+                       , _byDirectoryPath :: Maybe (Path -> Bool)    -- ^ Filter by directory path
+                       , _byDiffTree      :: Maybe (DiffTree -> Bool)-- ^ Filter by the `DiffTree`
+                       }
+
+makeLenses ''Wanted
+
+
+-- master:
+--  plugin list is input
+--  sorts plugins into simple/mapreduce
+--  makes pipes
+--  forks pipes
+--  connects pipes
+
+
+-- pipes
+--  (commit, [plugin]) producer
+--  byCommit filter
+--  (commit, difftree, [plugin]) <- which want difftrees
+--
+--  Don't forget an 'anyWants'
+--  byFileExtension >-> byFileName = byFile
+--
+-- details producer -> commit pipe -> \                                  / onlyCommit (commit, [plugin]) -> (plugin, commit) -> plugin cycle0
+--                         initialSort > (commit, [plugin]) -> byCommit <
+-- plugin  producer ----------------> /                                  \ diffTreePipe
+--
+--                                                          / onlyDiffTree (commit, difftree, [plugin]) -> (plugin, (commit, difftree)) -> plugin cycle1
+-- diffTreePipe (commit, difftree, [plugin]) -> byDiffTree <
+--                                                          \ dirTreePipe
+--
+--                                                         / onlyDirTree (commit, [difftree], dirtree, [plugin]) -> (plugin, (commit, [difftree], dirtree)) -> plugin cycle2
+-- dirTreePipe (commit, [difftree], dirtree, [plugin]) -> byDirTree <
+--                                                         \ filePipe
+--
+--                                                                   / onlyFiles (commit, [difftree], dirtree, file, [plugin]) -> (plugin, (commit, [difftree], ..)) -> plugin cycle3
+-- filePipe (commit, [difftree], dirtree, file, [plugin]) -> byFile <
+--                                                                   \ nullPipe
+--
+--
+--                                 / onlyStuffPipe
+-- forkedPipe x (x, _) -> byStuff <
+--                                 \ nextPipe
+--
+-- forkedPipe needs:
+--  up fork pipe
+--  down fork pipe
+--
+--  forkedPipe :: Pipe a b -> Pipe a c -> Consumer' a m ()
+--
+-- onlyStuffPipe (commit, .., [plugin]) -> (plugin, (commit, ..)) -> plugin cycle
+--
+--
+-- plugin cycle -> plugin1 -> plugin2 -> plugin3
+--                       \          |          /
+--                        \         |         /
+--                         \/       V       \/
+--                           results mailbox
+
+splitterPipe :: Pipe a (a, a)
+splitterPipe = Pipes.Prelude.Map $ join (,)
+
+producerToTQueue :: Producer b m r -> STM (TQueue b)
+
+whileM_ :: Monad m => (a -> m Bool) -> m a -> m a
+whileM_ p x = do
+  y <- x
+  t <- p y
+  if t
+  then do
+    whileM_ p x
+  else do
+    return y
+
+duplicateInput :: Input a -> Output a -> Output a -> STM ()
+duplicateInput input out1 out2 = whileM_ return duplicateOneOutput
+  where
+    duplicateOneOutput = do
+      maybeVal <- recv input
+      case maybeVal of
+        Just x  -> do
+          sent1 <- send out1 x
+          sent2 <- send out2 x
+          return $ sent1 && sent2
+        Nothing -> return False
+
+
+forkProducer :: (Consumer' b m r, Producer' b m r, Producer' b m r)
+forkProducer p = (consumer, producer1, producer2)
+  where
+
+(output0 , input0) <- spawn unlimited
+(output1, input1) <- spawn unlimited
+(output2, input2) <- spawn unlimited
+
+consumer = toOutput output0
+
+duplicateInput input0 output1 output2
+
+producer1 = toInput input1
+producer2 = toInput input2
+
+
+mergeProducers = Pipes.Prelude.zipWith
+
+-- idea design for splitter: (from left/right)
+--
+--
+--                                                                      / Producer b1
+-- Producer' (Either b1 b2) m r -> Pipe (Either b1 b2) (Either b1 b2) ->
+--                              /                                       \
+--                              \                                        |
+--                               <-Pipe (Either b1 b2) (Either b1 b2) <-/
+--                              /
+--                   Producer b2
+
+forkPipe :: Pipe a (a, a)
+forkPipe = Pipes.Prelude.map $ join (,)
+
+
+forkPipes :: Pipe a b -> Pipe c d -> Pipe (a, c) (b, d)
+forkPipes
+
+
+detailsProducer :: Producer Details
+commitPipe :: Pipe Details (Details, Commit)
+pluginProducer :: Producer Plugin
+initialSort :: Pipe () (Details, Commit, [Plugin])
+byCommit :: Pipe (Details, Commit, [Plugin]) (Details, Commit, [Plugin])
+
+
+
+predToMaybe :: (a -> Bool) -> b -> a -> Maybe b
+predToMaybe p x y | p y       = Just x
+                  | otherwise = Nothing
+
+predToList :: (a -> Bool) -> b -> a -> [b]
+predToList p x y | p y       = [x]
+                 | otherwise = []
+
+
+initialSorter :: (a -> b -> Bool) -> [b] -> a -> (a, [b])
+initialSorter p ys x = (x, filter (p x) ys)
+
+pipeInitialSorter :: (a -> b -> Bool) -> [b] -> Pipe a (a, [b])
+pipeInitialSorter p ys = Pipes.Prelude.map $ initialSorter p ys
+
+
+sorter :: (a -> b -> Bool) -> (a, [b]) -> (a, [b])
+sorter p ~(a, bs) = (a, filter (p a) bs)
+
+pipeSorter :: (a -> b -> Bool) -> Pipe (a, [b]) (a, [b])
+pipeSorter p = Pipes.Prelude.map $ sorter p
+
+-- | @maybePred pred x@ applies @pred@ if it's `Just` a predicate
+-- otherwise, it returns `True`
+maybePred :: Maybe (a -> Bool) -> a -> Bool
+maybePred (Just p) x = p x
+maybePred  _       _ = True
+
+commitProducer :: Producer Commit
+
+
+commitSorter :: [Plugin] -> Pipe Commit (Commit, [Plugin])
+commitSorter ps = pipeInitialSorter pred ps
+  where
+    pred c p = maybePred (p^.byCommit) c
+
+diffTreePipe :: Pipe Commit (Commit, DiffTree)
+diffTreePipe = Pipes.Prelude.mapM commitToDiffTrees
+
+
+-- HOW to balance load?
+-- Well, simply make a mailbox of commits
+-- assume that most of the load is in fetching
+-- consider multi-stage plugins with a fast merger (like counters, or sorting to merge)
+-- definately have a map/reduce plugin sorta thing
+-- This should be relatively quick once the basics are pumped out
+
 
 -- Filter order
 --  commit date
@@ -90,98 +275,4 @@ wantNothing = Want False False False False False n n n n n n n
   where
     n = Nothing
 
--- by commit hash/data
--- by filename
--- diffs
 
-
-
--- master
---  runs event loop (get, push to plugins, feedback, next commit)
---  manages fetching of files
---  updates global read variables
-
--- plugin
---  gets opened with results of parser, details
---  gets current commit, local attributes, dirtree, etc.
---  can request files (or marks files as wanted/unwanted?
---  somehow updates stuff, requests, and continues.
---  basically, has full access to local, read only to global, some local exported to update local
-
--- fetchers
---  gets stream of requests (in order, somehow)
---  streams out in hierarchy
-
--- So we have Map Plugin FileName.
--- Want to convert to Map FileName Plugin
---
--- Or, we have lots of (Plugin, Array (FileName, Bool))
--- Then merged, pretty easily to Array FileName [Plugin]
--- Then unmerged by streaming to each plugin
--- Also streamed much more easily
--- Yeah, I like these streams, very fast
--- Require that they're ordered, so that there's little blocking
---
-
--- filenames are max 255 bytes, make fixed length? hmmmm.... CStrLn is not too bad.....
-
--- (plugin) request :: LocalState -> Stream (FileName, Maybe PluginName)
--- merge :: Stream (FileName, Maybe PluginName) -> Stream (FileName, Maybe PluginName) -> Stream (FileName, [PluginName])
--- unmerge -- stream back to plugins
-
--- This is read-only on local level, updated only by master
-data CLILocalState  { details    :: Details
-                    , commit     :: Commit
-                    , dirtree    :: DirectoryTree
-                    , files      :: M.Map FilePath Text
-                    }
-
--- These are read/write, though local to each plugin
--- These may be read
-                    , requests   :: [FilePath]
-                    , attributes :: [Attribute]
-
--- This may only be accessed by master
-data CLIGlobalState { details    :: Details
-                    , commits    :: Commit
-                    , attributes :: Map PluginName [Attribute]
-                    , dirTree    :: DirectoryTree
-                    , requests   :: Map PluginName [FilePath]
-                    , files      :: M.Map Path Text
-                    }
-
-
-commitInfo :: Commit -> (DirectoryTree, Map FilePath Text)
-commitInfo
-
-mapToMap commitInfo
-
-data CLIState { details    :: Details
-              , commit     :: Commit
-              , commits    :: [Commit]
-              , attributes :: [Attribute]
-              , dirTree    :: DirectoryTree
-              , files      :: M.Map FilePath Text
-              }
-
--- Parse commands
--- For each active plugin,
---  for each commit.
---    pass commits to plugins
---    lazy contents to plugins
---    update CLIState
-
-
-data CLIState = CLIState { details    :: Details
-                         , commits    :: [Commit]
-                         , attributes :: [Attribute]
-                         , getFile    :: M.Map (Commit, FilePath) Text
-                         , getDirTree :: M.Map  Commit            DirectoryTree
-                         }
-
-Map Commit (DirectoryTree, Map FilePath Text)
-
-
-
-data Plugin a = { parser :: Parser a
-                ,
